64b9c6a864711532403b890627a584b84b958d85
JulienJandot <julienjandot@orange.fr>
2018-10-17 11:23:55 +0200

    tp2

--- TP2/TP2.cpp
+++ TP2/TP2.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
-#include <algorithm>
+#include <fstream>
 #include "../CImg.h"
 
 using namespace cimg_library;
-
-struct Pixel{
-    int x, y, z;
-};
-
-bool contains(std::vector<Pixel> l, Pixel p){
-    for(int i = 0; i < l.size(); ++i){
-        if(l[i].x == p.x && l[i].y == p.y && l[i].z == p.z)
-            return true;
-    }
-    return false;
-}
-
 
 int main(int argc, char *argv[]){  
     int cpt = 0;
@@ -25,9 +11,7 @@ int main(int argc, char *argv[]){
     CImg<> img("liver_07.liver.norm.hdr");
     CImgDisplay display(img, "liver_07.liver.norm.hdr");
     
-    int currentGrayLevel;
-    std::vector<Pixel> l;
-    int lCpt = 0;
+    std::ofstream histo("histo.dat");
     
     while (!display.is_closed()) {
         if(display.is_keyESC())
@@ -39,40 +23,22 @@ int main(int argc, char *argv[]){
             
             std::cout << "Clicked at : x = " << x << ", y = " << y << ". Gray level is : " << img(x, y, cpt) << std::endl;
             
-            Pixel px;
-            px.x = x;
-            px.y = y;
-            px.z = cpt;
-            
-            
-            l.push_back(px);
-
-            int nbAdd;
-            while(true){
-                nbAdd = 0;
-                for(int index = lCpt; index < l.size(); ++index){
-                    lCpt++;
-                
-                    for(int i = -1; i <= 1; ++i){
-                        for(int j = -1; j <= 1; ++j){
-                            for(int k = -1; k <= 1; ++k){
-                                if(i == j == k == 0)
-                                    continue;
-                                if(img(l[index].x + i, l[index].y + j, l[index].z + k) >= currentGrayLevel){
-                                    Pixel p{l[index].x + i, l[index].y + j, l[index].z + k};
-                                    if(!contains(l, p)) {
-                                        l.push_back(p);
-                                        ++nbAdd;
-                                    }
-                                }
-                            }
-                        }
+            for(int i = 0; i <= 255; ++i){
+                CImg<> image = img;
+                CImg<> region;
+                int regionSize = 0;
+                unsigned char white[] = {255, 255, 255};
+                image.draw_fill(x, y, cpt, white, 1, region, i);
+                for(int j = 0; j < region.width(); ++j){
+                    for(int k = 0; k < region.height(); ++k){
+                        if(region(j, k) > 0)
+                            ++regionSize;
                     }
                 }
-                if(nbAdd == 0)
-                    break;
+                histo << i << " " << regionSize << std::endl;
             }
-            
+            std::cout << "End procesing" << std::endl;
+            histo.close();
         }
         
         if(display.wheel()){
